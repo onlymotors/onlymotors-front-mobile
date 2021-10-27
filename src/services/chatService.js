6f1e735b424881451279cfa8e0d69fd1d3865d69
Diego Rodrigo da Silva <diegorodrigocpv@outlook.com>
2021-10-27 08:51:19 -0300

    Adicionando serviço de Chat

---
+++ src/services/chatService.js
@@ -0,0 +1,41 @@
+import io from 'socket.io-client';
+import { getToken } from './tokenService';
+
+let socket;
+let token;
+getToken()
+  .then(res => {
+    token = res
+  })
+  .catch(e => {
+
+  })
+console.log(token)
+
+export const initiateSocket = (room) => {
+
+  socket = io('http://localhost:3333/chat', {
+    extraHeaders: { "Authorization": `Bearer ${token}` },
+    // transports: ['websocket'],
+    jsonp: false
+  });
+  console.log(`Connecting socket...`);
+  if (socket && room) socket.emit('join', room);
+}
+
+export const disconnectSocket = () => {
+  console.log('Disconnecting socket...');
+  if (socket) socket.disconnect();
+}
+
+export const subscribeToChat = (cb) => {
+  if (!socket) return (true);
+  socket.on('chat', msg => {
+    console.log('Websocket event received!');
+    return cb(null, msg);
+  });
+}
+
+export const sendMessage = (dados) => {
+  if (socket) socket.emit('chat', dados);
+}
