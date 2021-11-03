1f9d77754fd3ee32f777b2c78ce8ebf546a524ca
Diego Rodrigo da Silva <diegorodrigocpv@outlook.com>
2021-11-03 14:39:53 -0300

    Trocando AsyncStorage por SecureStore

--- src/services/tokenService.js
+++ src/services/tokenService.js
@@ -1,22 +1,26 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as SecureStore from 'expo-secure-store';
 
 export async function getToken() {
   try {
-    return await AsyncStorage.getItem('token');
+    // return await AsyncStorage.getItem('token');
+    return await SecureStore.getItemAsync('token');
   } catch (e) {
   }
 }
 
 export async function setToken(value) {
   try {
-    return await AsyncStorage.setItem('token', value)
+    // return await AsyncStorage.setItem('token', value)
+    return await SecureStore.setItemAsync('token', value)
   } catch (e) {
   }
 }
 
 export async function clearToken() {
   try {
-    return await AsyncStorage.clear()
+    // return await AsyncStorage.clear()
+    return await SecureStore.deleteItemAsync('token')
   } catch (e) {
   }
 }
